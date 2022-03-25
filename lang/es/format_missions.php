a7b8efd25c087aae7618113a418cf2e615a0ddd5
John Moravec <51122406+educationfutures@users.noreply.github.com>
2022-03-25 09:11:08 -0500

    Added ES placeholder file for translation

---
+++ lang/es/format_missions.php
@@ -0,0 +1,24 @@
+<?php
+$string['pluginname'] = 'Missions format'; //NOTE: FILE NEEDS TO BE TRANSLATED
+$string['page-course-view-missions'] = 'Any course main page in missions format';
+$string['page-course-view-missions-x'] = 'Any course page in missions format';
+$string['hidefromothers'] = 'Hide mission';
+$string['showfromothers'] = 'Show mission';
+$string['automaticenddate'] = 'Calculate the end date from the number of sections';
+$string['automaticenddate_help'] = 'If enabled, the end date for the course will be automatically calculated from the number of sections and the course start date.';
+$string['privacy:metadata'] = 'The mission format plugin does not store any personal data.';
+
+$string['addsections'] = 'Add missions';
+$string['currentsection'] = 'This mission';
+$string['editsection'] = 'Edit mission';
+$string['editsectionname'] = 'Edit mission name';
+$string['sectionname'] = 'Mission';
+$string['section0name'] = 'Training';
+$string['newsectionname'] = 'New name for mission {$a}';
+$string['deletesection'] = 'Delete mission';
+
+$string['missions_outline'] = 'Missions outline';
+$string['is_sidequest'] = 'Mission is a side quest';
+$string['yes'] = 'Yes';
+$string['no'] = 'No';
+$string['is_sidequest_help'] = 'If checked, differentiates the mission point on the Mission Map block';
